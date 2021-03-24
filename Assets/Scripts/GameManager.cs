ccec5b850532d4829bb0c73cc0625f6a14bbf847
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-24 15:55:26 +0100

    Make GameOver text appear

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
     private int score;
     private float spawnRate = 1.0f;
 
@@ -17,6 +18,8 @@ public class GameManager : MonoBehaviour
         score = 0;
 
         UpdateScore(0);
+
+        gameOverText.gameObject.SetActive(true);
         
     }
 
