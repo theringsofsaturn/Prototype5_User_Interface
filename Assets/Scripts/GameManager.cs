95aba69fa914144e8bd0d14e9da4d44a6ce8d697
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-03-24 03:59:19 +0100

    Create a coroutine to spawn objects

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    private float spawnRate;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
@@ -17,4 +18,16 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    
 }
